160ea184364684d988cc221c2bea6418ed64b5b9
Michael Redenti <m.redenti@cineca.it>
2025-03-12 17:13:39 +0100

    BreakBeforeBraces: Attach -> braces are attached to the same line instead of breaking onto a new line

--- scripts/format_code.sh
+++ scripts/format_code.sh
@@ -3,5 +3,11 @@
 # Get the absolute path of the script's directory
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+# Get the parent directory (assumes the script is in the "scripts" folder)
+PARENT_DIR=$(realpath "$SCRIPT_DIR/..")
+
+# Export the parent directory as the clang-format configuration directory
+CLANG_FORMAT_STYLE="file:$PARENT_DIR/.clang-format"
+
 # Find all .cpp and .h files and format them using clang-format
-find "$SCRIPT_DIR/.." -name '*.cpp' -o -name '*.h' | xargs clang-format -i
+find "$PARENT_DIR" -name '*.cpp' -o -name '*.h' | xargs clang-format -i --style="$CLANG_FORMAT_STYLE"
