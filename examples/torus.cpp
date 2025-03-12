57c6bb20218b12555f9c4266c8d3502467efe8ff
Michael Redenti <m.redenti@cineca.it>
2025-03-12 17:38:54 +0100

    rename example to torus

---
+++ examples/torus.cpp
@@ -0,0 +1,18 @@
+
+import mesh;
+#include <iostream>
+
+int main() {
+    Mesh mesh(10);
+
+    for (int i = 0; i < mesh.getSize(); ++i) {
+        mesh.setValue(i, i * 10);
+    }
+
+    for (int i = 0; i < mesh.getSize(); ++i) {
+        std::cout << "Value at index " << i << ": " << mesh.getValue(i)
+                  << std::endl;
+    }
+
+    return 0;
+}
